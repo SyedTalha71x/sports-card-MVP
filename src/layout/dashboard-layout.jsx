43c8857267f5c6158c816d6bafe2ee2929c06482
SyedTalha71x <syedtalha71x@gmail.com>
2025-02-08 19:47:56 +0500

    update fonts

--- src/layout/dashboard-layout.jsx
+++ src/layout/dashboard-layout.jsx
@@ -6,7 +6,7 @@ import ProfileSVG from '../../public/PROFILE.svg'
 
 const DashboardLayout = () => {
   return (
-    <div className="min-h-screen flex flex-col lg:mt-[8%] mt-[15%]">
+    <div className="min-h-screen flex flex-col lg:mt-[10%] md:mt-[15%] sm:mt-[50%] mt-[50%] px-4 md:px-0 pb-10">
     <div className="flex-1 container mx-auto px-4 py-8 max-w-6xl">
       <h1 className="text-5xl hero_h1 text-center flex justify-center items-center flex-col mb-2"> <img src={ProfileSVG} alt="" />My Account</h1>
       <p className="text-center text-gray-600 mb-8">
--- src/partials/my-orders.jsx
+++ src/partials/my-orders.jsx
@@ -33,26 +33,26 @@ const OrderTable =()  =>{
       <table className="min-w-full border-collapse">
         <thead>
           <tr className="text-left">
-            <th className="px-3 py-2 text-sm font-medium hero_h1 text-gray-500">ORDER ID</th>
-            <th className="px-3 py-2 text-sm font-medium hero_h1 text-gray-500">DATE & TIME</th>
-            <th className="px-3 py-2 text-sm font-medium hero_h1 text-gray-500">TOTAL PRICE</th>
-            <th className="px-3 py-2 text-sm font-medium hero_h1 text-gray-500">STATUS</th>
-            <th className="px-3 py-2 text-sm font-medium hero_h1 text-gray-500">ACTION</th>
+            <th className="px-3 py-2 lg:text-sm text-[12px] font-medium hero_h1 text-gray-500">ORDER ID</th>
+            <th className="px-3 py-2 lg:text-sm text-[12px] font-medium hero_h1 text-gray-500">DATE & TIME</th>
+            <th className="px-3 py-2 lg:text-sm text-[12px] font-medium hero_h1 text-gray-500">TOTAL PRICE</th>
+            <th className="px-3 py-2 lg:text-sm text-[12px] font-medium hero_h1 text-gray-500">STATUS</th>
+            <th className="px-3 py-2 lg:text-sm text-[12px] font-medium hero_h1 text-gray-500">ACTION</th>
           </tr>
         </thead>
         <tbody>
           {orders.map((order, index) => (
             <tr key={index} className="border-t border-gray-100">
-              <td className="px-4 py-3 text-sm  font-semibold">{order.id}</td>
-              <td className="px-4 py-3 text-sm text-gray-800 font-semibold">{order.dateTime}</td>
-              <td className="px-4 py-3 text-sm text-gray-800 font-semibold">{order.price}</td>
+              <td className="px-4 py-3 lg:text-sm text-[12px]  font-semibold">{order.id}</td>
+              <td className="px-4 py-3 lg:text-sm text-[12px] text-gray-800 font-semibold">{order.dateTime}</td>
+              <td className="px-4 py-3 lg:text-sm text-[12px] text-gray-800 font-semibold">{order.price}</td>
               <td className="px-4 py-3">
-                <span className="inline-flex items-center px-2.5 py-1.5 rounded-full text-xs font-medium  text-gray-600 border border-[#987C5D] cursor-pointer">
+                <span className="inline-flex items-center px-2.5 py-1.5 rounded-full lg:text-xs text-[12px] font-medium  text-gray-600 border border-[#987C5D] cursor-pointer">
                   {order.status}
                 </span>
               </td>
               <td className="px-4 py-3">
-                <button className="px-4 py-2 text-xs font-medium text-white bg-[#987C5D] rounded-2xl cursor-pointer transition-colors">
+                <button className="px-4 py-2 lg:text-xs text-[12px] font-medium text-white bg-[#987C5D] rounded-2xl cursor-pointer transition-colors">
                   Track Order
                 </button>
               </td>
