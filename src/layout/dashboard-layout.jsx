2759cafb8daad48da4960c69d781c47e1ab64cd1
SyedTalha71x <syedtalha71x@gmail.com>
2025-02-08 19:50:14 +0500

    update fonts

--- src/layout/dashboard-layout.jsx
+++ src/layout/dashboard-layout.jsx
@@ -8,7 +8,7 @@ const DashboardLayout = () => {
   return (
     <div className="min-h-screen flex flex-col lg:mt-[10%] md:mt-[15%] sm:mt-[50%] mt-[50%] px-4 md:px-0 pb-10">
     <div className="flex-1 container mx-auto px-4 py-8 max-w-6xl">
-      <h1 className="text-5xl hero_h1 text-center flex justify-center items-center flex-col mb-2"> <img src={ProfileSVG} alt="" />My Account</h1>
+      <h1 className="lg:text-5xl text-4xl hero_h1 text-center flex justify-center items-center flex-col mb-2"> <img src={ProfileSVG} alt="" />My Account</h1>
       <p className="text-center text-gray-600 mb-8">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun
       </p>
